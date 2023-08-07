1475e04fde57f5e7e8cd9af86c48e810f2d8693f
AleixBP <abp.21o@gmail.com>
2023-08-07 09:53:58 +0200

    make imports more general

--- rdsplines/rdspline_fit.py
+++ rdsplines/rdspline_fit.py
@@ -1,21 +1,24 @@
 import sys
 from rdsplines import array_lib as np
-import bs_utils as bu
-from descent_algorithms import APGD
-from bs_Hessian import bs_Hessian
-from HessianSchattenNorm import LpSchattenNorm, HessianSchattenNorm
+import rdsplines.bs_utils as bu
+from rdsplines.descent_algorithms import APGD
+from rdsplines.bs_Hessian import bs_Hessian
+from rdsplines.HessianSchattenNorm import LpSchattenNorm, HessianSchattenNorm
 
-def rdspline_fit(scaled_coords, coeffs_shape, bcs, measure_func = lambda xys: np.ones_like(xys[...,0]),
+def rdspline_fit(measured_coords, coeffs_shape, bcs, measure_func = lambda xys: np.ones_like(xys[...,0]),
                     deg=1, integration_scale=1, init_coeffs=None, prox_iter=50, lam=10., apgd_iter=50, verbose=0):
     
     # Initialize
     if init_coeffs is None: init_coeffs = -np.ones(coeffs_shape)
     step = 1/integration_scale
-    measure = measure_func(np.mgrid[0:coeffs_shape[1]:step, 0:coeffs_shape[0]:step].T).T
-    measure_s = measure_func(np.mgrid[0:coeffs_shape[1], 0:coeffs_shape[0]].T).T
+    #measure = measure_func(np.mgrid[0:coeffs_shape[1]:step, 0:coeffs_shape[0]:step].T).T
+    #measure_s = measure_func(np.mgrid[0:coeffs_shape[1], 0:coeffs_shape[0]].T).T
+    grid_ranges = [np.s_[0:axis_length:step] for axis_length in coeffs_shape[::-1]]
+    measure = measure_func(np.mgrid[grid_ranges].T).T
+    measure_s = measure_func(np.mgrid[grid_ranges].T).T
 
     # Create likelihood
-    nlsl = bu.neg_rdspline_likelihood(scaled_coords, coeffs_shape, deg, integration_scale, bcs,
+    nlsl = bu.neg_rdspline_likelihood(measured_coords, coeffs_shape, deg, integration_scale, bcs,
                                     measure=measure, measure_s=measure_s,
                                     updater={"approx":False, "u_multi":1., "use_circles":True})
     
@@ -34,7 +37,7 @@ def rdspline_fit(scaled_coords, coeffs_shape, bcs, measure_func = lambda xys: np
     Hessian_Schatt_Norm = HessianSchattenNorm(Hessian, Schatt_Norm, prox_iter, lam)
 
     # Accelerated proxima-gradient descent
-    out_coeffs = APGD(nlsl, Hessian_Schatt_Norm, niter=apgd_iter, verbose=verbose, update_ilip=True, restart_method="RESTART")
+    out_coeffs = APGD(nlsl, Hessian_Schatt_Norm, x0=init_coeffs, niter=apgd_iter, verbose=verbose, update_ilip=True, restart_method="RESTART")
     
     out_eval = bu.eval_rdspline_on_subgrid(out_coeffs, deg, 1, bcs)/nlsl.density_integral
     
